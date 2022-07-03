9e9fa156d4ed307a6ab5a56bba228837f66e0899
MPataro <pataro.miguel@gmail.com>
2022-07-03 03:59:43 -0300

    Exercício 1 - Não sei se a forma correta, porém funfo rs

--- Exercicios/Ex1.js
+++ Exercicios/Ex1.js
@@ -3,4 +3,13 @@ const generateValuesArray = require("../helpers");
 // 1 – Calcule a média de diversas notas pelo usuário.
 const notas = generateValuesArray({ length: 10 });
 
+
+
 console.table(notas);
+console.log(
+    notas
+        .reduce(
+            (prevValue, currentValue) => (prevValue + currentValue), notas[0]
+        ) / notas.length,
+
+);
